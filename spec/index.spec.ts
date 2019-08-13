b462250522f524e85b8efb2daa7d60f5fde02d3b
gmclp <gchampion@liveperson.com>
2019-08-13 12:12:00 -0400

    Reenable unit tests

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -1,4 +1,4 @@
-import { createBasesFromColor, getRgbDifference, getRgbFromHexString, rgbNumberToString, rgbStrings } from "../src/";
+import { createBasesFromColor, getRgbDifference, getRgbFromString, rgbNumberToString, rgbStrings } from "../src/";
 describe("solarizer", () => {
   describe("rgbNumberToString", () => {
     it("should convert 3 number arrays into rgb strings", async () => {
@@ -6,13 +6,13 @@ describe("solarizer", () => {
       expect(rgbString).toEqual("rgb(1,2,3)");
     });
   });
-  describe("getRgbFromHexString", () => {
+  describe("getRgbFromString", () => {
     it("should properly convert hex with # to rgb", async () => {
-      const rgb = getRgbFromHexString("#001122");
+      const rgb = getRgbFromString("#001122");
       expect(rgb).toEqual([0, 17, 34]);
     });
     it("should properly convert hex without # to rgb", async () => {
-      const rgb = getRgbFromHexString("001122");
+      const rgb = getRgbFromString("001122");
       expect(rgb).toEqual([0, 17, 34]);
     });
   });
@@ -22,9 +22,29 @@ describe("solarizer", () => {
       expect(difference).toEqual([6, 6, 6]);
     });
   });
-  describe.only("createBasesFromColor", () => {
+  describe("createBasesFromColor", () => {
     it("should be able to create bases for yellow", async () => {
       const bases = createBasesFromColor(rgbStrings.yellow, "base03");
+      expect(Object.values(bases).length).toEqual(9);
+      for (const baseKey of Object.keys(bases)) {
+        const base = bases[baseKey];
+        if (typeof base === "string") {
+          expect((base as string).includes("NaN")).toBeFalsy();
+        }
+      }
+    });
+    it("should be able to create bases for base01", async () => {
+      const bases = createBasesFromColor(rgbStrings.base01, "base03");
+      expect(Object.values(bases).length).toEqual(9);
+      for (const baseKey of Object.keys(bases)) {
+        const base = bases[baseKey];
+        if (typeof base === "string") {
+          expect((base as string).includes("NaN")).toBeFalsy();
+        }
+      }
+    });
+    it("should be able to create bases for orange", async () => {
+      const bases = createBasesFromColor(rgbStrings.orange, "base03");
       expect(Object.values(bases).length).toEqual(9);
       for (const baseKey of Object.keys(bases)) {
         const base = bases[baseKey];
